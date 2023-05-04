1235107f5f71e04402625d6a082c69d94124954e
Dani G. T <dgomeztorquemada@gmail.com>
2023-05-04 22:23:30 +0200

    Added and modified POJOS for first requisites

--- src/main/java/com/dgomezt/practicaintegradora/entities/helpers/Auditory.java
+++ src/main/java/com/dgomezt/practicaintegradora/entities/helpers/Auditory.java
@@ -21,24 +21,27 @@ public class Auditory {
     @Column(name = "entry_date")
     private LocalDate entryDate;
 
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "entry_user_id",
+            foreignKey = @ForeignKey(name = "FK_auditory_entryUser"))
+    private User entryUser;
+
     @Column(name = "last_modification_date")
     private LocalDate lastModificationDate;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "last_modification_user_id",
+            foreignKey = @ForeignKey(name = "FK_auditory_lastModificationUser"))
+    private User lastModificationUser;
 
     @Column(name = "removed_date")
     private LocalDate removedDate;
 
     @ToString.Exclude
     @ManyToOne
-    @JoinColumn(name = "entry_user_id")
-    private User entryUser;
-
-    @ToString.Exclude
-    @ManyToOne
-    @JoinColumn(name = "last_modification_user_id")
-    private User lastModificationUser;
-
-    @ToString.Exclude
-    @ManyToOne
-    @JoinColumn(name = "removed_user_id")
+    @JoinColumn(name = "removed_user_id",
+            foreignKey = @ForeignKey(name = "FK_auditory_removedUser"))
     private User removedUser;
 }
